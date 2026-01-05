2f98c2b4af3ddeaebcace1fa9765ef0b6ed58fa1
Josiah Brown <josiahd7.brown@gmail.com>
2026-01-05 14:29:54 -0500

    Had previously redefined the page layout in each route, now I just let app.html do that and each route contains only content

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,17 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Josiah Brown</title>
-  </head>
-  <body>
-    <h1>Josiah Brown's Personal Website!</h1>
-    <p>Test Route: <a href="http://localhost:5174/testRoute">Test Route!</a></p>
-    <p>Svelte documentation: <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-  </body>
-</html>
+<svelte:head>
+  <title>Josiah Brown</title>
+</svelte:head>
+
+<h1>Josiah Brown's Personal Website!</h1>
+
+<p>
+  Test Route:
+  <a href="/testRoute">Test Route!</a>
+</p>
+
+<p>
+  Svelte documentation:
+  <a href="https://svelte.dev/docs/kit">
+    svelte.dev/docs/kit
+  </a>
+</p>
--- frontend/src/routes/testRoute/+page.svelte
+++ frontend/src/routes/testRoute/+page.svelte
@@ -1,12 +1,7 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Route</title>
-  </head>
-  <body>
-    <h2>Nothing Here!</h2>
-    <p>This is a test route with no content.</p>
-  </body>
-</html>
+<svelte:head>
+    <title>Zilch</title>
+</svelte:head>
+
+<h2>Nothing Here!</h2>
+<p>This is a test route with no content.</p>
+
